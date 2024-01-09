3b66e4f98076ce973437c9ca2f541fcb89ddbfff
UmaBharathiMuniyappa <umahsk06@gmail.com>
2024-01-09 19:42:34 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def htmlforms(request):
+    return render(request,'htmlforms.html')
+
+def htmlforms1(request):
+    return render(request,'htmlforms1.html')
